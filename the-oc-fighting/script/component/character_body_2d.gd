70beea3e9ccbd3774738551cdfe96ec8669d2ed6
Oak Platinum <oakplatinum@qq.com>
2025-04-04 22:48:01 +0800

    dodge and parry

--- the-oc-fighting/script/component/character_body_2d.gd
+++ the-oc-fighting/script/component/character_body_2d.gd
@@ -1,38 +1,79 @@
 extends CharacterBody2D
 
+@export var SPEED = 300.0
+@export var JUMP_VELOCITY = -400.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+enum STATE{
+	IDLE,
+	MOVING,
+	DODGING,
+	PARRYING,
+}
+
+var state := STATE.IDLE
 
 func _ready() -> void:
 	pass
 
-
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# 重力处理
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	
+	handle_combat_actions()
+	
+	# 只在非防御/闪避状态允许跳跃和移动
+	if state == STATE.IDLE or state == STATE.MOVING:
+		handle_jump()
+		handle_movement(delta)
+	
+	
+	update_animation_state()
+	
+	move_and_slide()
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+func handle_jump():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+func handle_movement(delta):
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
+	
+	# 更新角色朝向
 	if velocity.x > 0:
 		$AnimatedSprite2D.flip_h = true
 	elif velocity.x < 0:
 		$AnimatedSprite2D.flip_h = false
+		
+	state = STATE.IDLE if velocity.x == 0 else STATE.MOVING 
+
+func handle_combat_actions():
+	if Input.is_action_just_pressed("dodge") and is_on_floor():
+		velocity.x = 0
+		state = STATE.DODGING
+		$AnimatedSprite2D.play("dodge")
+			
+	if Input.is_action_just_released("dodge"):
+		state = STATE.IDLE
 	
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if state == STATE.DODGING and (
+		direction > 0 and not $AnimatedSprite2D.flip_h or 
+		direction < 0 and $AnimatedSprite2D.flip_h):
+			state = STATE.PARRYING
+			$AnimatedSprite2D.play("parry")
+
+func update_animation_state():
+	# 优先处理战斗动作动画
+	if not (state == STATE.IDLE or state == STATE.MOVING):
+		return
+	
+	# 常规动画状态
 	if abs(velocity.x) > 0:
 		$AnimatedSprite2D.play("move")
 	else:
 		$AnimatedSprite2D.play("idle")
-
-	move_and_slide()
