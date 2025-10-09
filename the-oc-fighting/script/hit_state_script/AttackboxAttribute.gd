cdafe074911cc57ec56a962b45e58f20261a23ef
QiaokunLiu <1072031285@qq.com>
2025-10-09 19:38:49 +0800

    Attribute修改
    修改FrayHitAttribute，目前能够正常运行攻击和受击的逻辑

--- the-oc-fighting/script/hit_state_script/AttackboxAttribute.gd
+++ the-oc-fighting/script/hit_state_script/AttackboxAttribute.gd
@@ -8,13 +8,8 @@ func _get_color_impl() -> Color:
 
 # 定义核心规则：这个攻击框应该检测谁？
 func _allows_detection_of_impl(other_attribute: FrayHitboxAttribute) -> bool:
-	# 这里应该有问题，应该需要进行同源检测？但同源检测的代码在FrayHitbox脚本中的can_detected()中。
-	# 如果碰到的也是玩家自己的攻击，则不检测（防止自己的拳头打自己的另一个拳头）
 	if other_attribute is AttackAttribute:
-		return false
-	# 如果碰到的是玩家自己的受击框，也不检测（防止打自己）
-	if other_attribute is HurtboxAttribute:
-		return false
+		return true
 
 	# 除此之外，允许检测所有其他东西（比如敌人的受击框）
-	return true
+	return false
--- the-oc-fighting/script/hit_state_script/HurtboxAttribute.gd
+++ the-oc-fighting/script/hit_state_script/HurtboxAttribute.gd
@@ -8,4 +8,6 @@ func _get_color_impl() -> Color:
 
 # 接收另一个FrayHitbox的FrayAttribute，进行判断返回是否可以允许检测，默认为true
 func _allows_detection_of_impl(other_attribute: FrayHitboxAttribute) -> bool:
+	if other_attribute is HurtboxAttribute:
+		return false
 	return true
